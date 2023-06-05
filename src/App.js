3eceeb709621f4ce9e720214e7aca19ed127e171
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-06-05 21:04:46 +0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
+import Headers from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
+import AppRouter from './components/AppRouter';
 
 
 function App() {
-    return (
-        <div className="app">
 
-        </div>
+
+    return (
+        <BrowserRouter>
+            <Headers />
+
+            <div className="app">
+                <AppRouter />
+            </div>
+
+        </BrowserRouter>
     );
 }
 
