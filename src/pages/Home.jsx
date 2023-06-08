62258740d2b0f4d70072f49445331f293a1a582a
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-06-08 21:06:39 +0500

    add title text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import GridList from '../components/GridList';
-import { fullPdfList } from '../config';
+import { fullPdfList, homeTitle, homeDescription } from '../config';
 import '../styles/Home.css';
+import Text from '../components/UI/Text/Text';
+import { Panel } from 'rsuite';
 
 
 function Home() {
-
-
     return (
         <div className='home'>
-            Home
-            <GridList items={fullPdfList} big />
+            <div className='home__title'>
+                <Text size='lg'>{homeTitle}</Text>
+                <Text size='sm'>{homeDescription}</Text>
+            </div>
+            <Panel className='home__panel' bordered shaded >
+                <GridList items={fullPdfList} big />
+            </Panel>
         </div>
     );
 }
