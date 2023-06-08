4b2c9c85438f7bbec8179aada317f746b82ea8ef
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-06-08 19:54:15 +0500

    lists were taken out in the config.js

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,39 +3,29 @@ import { Navbar, Nav, FlexboxGrid } from 'rsuite';
 import '../styles/Header.css';
 import Icon from './UI/Icon/Icon';
 import GridList from './GridList';
+import { Link } from 'react-router-dom';
+import { fromPdfList, toPdfList } from '../config';
 
 
 function Header() {
-    const toPdfList = [
-        { title: 'JPG в PDF', image: <Icon picture={'image.svg'} />, link: '/jpg-pdf' },
-        { title: 'WORD в PDF', image: <Icon picture={'doc.svg'} />, link: '/docx-pdf' },
-        { title: 'POWERPOINT в PDF', image: <Icon picture={'ppt.svg'} />, link: '/pptx-pdf' },
-        { title: 'EXCEL в PDF', image: <Icon picture={'xls.svg'} />, link: '/xlsx-pdf' },
-        { title: 'HTML в PDF', image: <Icon picture={'html.svg'} />, link: '/html-pdf' },
-    ]
-
-    const fromPdfList = [
-        { title: 'PDF в JPG', image: <Icon picture={'image.svg'} />, link: '/pdf-jpg' },
-        { title: 'PDF в WORD', image: <Icon picture={'doc.svg'} />, link: '/pdf-docx' },
-        { title: 'PDF в POWERPOINT', image: <Icon picture={'ppt.svg'} />, link: '/pdf-pptx' },
-        { title: 'PDF в EXCEL', image: <Icon picture={'xls.svg'} />, link: '/pdf-xlsx' },
-    ]
-
-
     return (
         <Navbar className='header'>
             <div className='container'>
-                <Navbar.Brand>
-                    <Icon picture={'temp.svg'} />
-                </Navbar.Brand>
-
+                <Link to='/'>
+                    <Navbar.Brand as='div'>
+                        <Icon picture={'temp.svg'} />
+                    </Navbar.Brand>
+                </Link>
                 <Nav>
-                    <Nav.Item>О НАС</Nav.Item>
-
+                    <Link to='/about'>
+                        <Nav.Item as='div'>
+                            О НАС
+                        </Nav.Item>
+                    </Link>
                     <Nav.Menu trigger='hover' title="КОНВЕРТИРОВАТЬ PDF">
                         <FlexboxGrid className='header__flex-box'>
-                            <GridList items={toPdfList} title="КОНВЕРТАЦИЯ В PDF" />
-                            <GridList items={fromPdfList} title="ПРЕОБРАЗОВАТЬ ИЗ PDF" />
+                            <GridList size={'sm'} items={toPdfList} title="КОНВЕРТАЦИЯ В PDF" />
+                            <GridList size={'sm'} items={fromPdfList} title="ПРЕОБРАЗОВАТЬ ИЗ PDF" />
                         </FlexboxGrid>
                     </Nav.Menu>
                 </Nav>
