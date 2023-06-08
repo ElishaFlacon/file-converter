8e519b0da46992f1f8bf279232f9d9a5f6567997
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-06-08 21:48:01 +0500

    update about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import Text from '../components/UI/Text/Text';
+import { aboutText } from '../config';
 
 
 function About() {
-
-
     return (
         <div>
-            About
+            <Text size='lg'>О нас</Text>
+            <Text size='sm'>{aboutText}</Text>
         </div>
     );
 }
