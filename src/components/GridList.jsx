168d2085e1ad36547d4213397b461abd0959c12b
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-06-08 19:50:22 +0500

    update grid list now it can be better reused

--- src/components/GridList.jsx
+++ src/components/GridList.jsx
@@ -2,31 +2,35 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, FlexboxGrid } from 'rsuite';
 import '../styles/GridList.css';
+import Icon from './UI/Icon/Icon';
 
 
-function GridList({ items, title }) {
+function GridList({ items, title, big }) {
     return (
-        <FlexboxGrid className='grid-list'>
-            {title && <div className='grid-list__title'>{title}</div>}
+        <FlexboxGrid className={big ? 'grid-list-b' : 'grid-list-s'} >
+            {title && <div className='grid-list__title-s'>{title}</div>}
 
             {items.map((item) => {
                 return (
-                    <FlexboxGrid.Item key={item.title}>
-                        <Link to={item.link}>
+                    <FlexboxGrid.Item
+                        className={big ? 'grid-list__item' : ''}
+                        key={item.link}
+                    >
+                        <Link to={item.link} className={big ? 'grid-list__link' : 'grid-list__button-s'}>
                             <Button
-                                className='grid-list__button'
-                                appearance='subtle'
-                                startIcon={item.image}
-                                block
+                                className={big ? 'grid-list__button-b' : 'grid-list__button-s'}
+                                startIcon={<Icon picture={item.image} size={big ? 'lg' : 'sm'} />}
                             >
-                                {item.title}
+                                <div className={big ? 'grid-list__title-b' : ''}>
+                                    {item.title}
+                                </div>
+                                {big && <div className='grid-list__text'>{item.text}</div>}
                             </Button>
                         </Link>
                     </FlexboxGrid.Item>
                 );
             })}
-
-        </FlexboxGrid >
+        </ FlexboxGrid>
     );
 }
 
