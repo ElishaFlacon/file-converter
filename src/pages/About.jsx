b56bd9059e401377f3c0273049d41e57fef2eaea
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-06-09 00:00:09 +0500

    add class about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Text from '../components/UI/Text/Text';
 import { aboutText } from '../config';
+import '../styles/About.css';
 
 
 function About() {
     return (
-        <div>
+        <div className='about'>
             <Text size='lg'>О нас</Text>
             <Text size='sm'>{aboutText}</Text>
         </div>
