ce74d6298862407e5b3b8191d90171f1d0c208d2
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-06-23 16:10:33 +0500

    add undefind page

---
+++ src/pages/Undefind/Undefind.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Text from '../../components/UI/Text/Text';
+import classes from './Undefind.module.css';
+import { Link } from 'react-router-dom';
+import { Button } from 'rsuite';
+
+
+function Undefind() {
+    return (
+        <div className={classes.undefind}>
+            <Text size='xl'>Упс!</Text>
+            <Text size='lg'>Такой страницы не существует 😔</Text>
+            <Link className={classes.link} to={'/'}>
+                <Button appearance="link">
+                    <Text size='sm'>вернуться на главную страницу</Text>
+                </Button>
+            </Link>
+        </div>
+    );
+}
+
+
+export default Undefind;
