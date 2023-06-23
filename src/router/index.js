b3df67a150d2c34e77ed507b253ecf3e73ca6806
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-06-23 16:17:43 +0500

    add undefind to routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
-import Home from '../pages/Home';
-import About from '../pages/About';
-import Convertation from '../pages/Convertation';
+import Home from '../pages/Home/Home';
+import About from '../pages/About/About';
+import Convertation from '../pages/Convertation/Convertation';
+import Undefind from '../pages/Undefind/Undefind';
 
 
 export const routes = [
     { path: '/', component: Home },
     { path: '/about', component: About },
+    { path: '/undefind', component: Undefind },
     { path: '/:convert', component: Convertation },
 ]
