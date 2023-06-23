7c9099e9ec0385988836d69ae89304431d8d63f5
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-06-23 16:09:20 +0500

    rename function

--- src/service/Path.service.js
+++ src/service/Path.service.js
@@ -1,10 +1,10 @@
 import { fullPdfList } from "../config";
 
 
-export default class Path {
-    // интерцептор, который проверяет ссылку по которой переходит пользователь
-    // чтобы не было переходов по не существующим ссылкам (ссылкам для которых нет функционала, типо pdf-pdf)
-    static convertInterceptor(path) {
+export default class PathService {
+    // проверяем ссылку по которой переходит пользователь
+    // чтобы не было переходов по ссылкам для которых нет функционала, типо pdf-pdf
+    static validation(path) {
         for (let index = 0; index < fullPdfList.length; index++) {
             // удаляем 1 букву, т.к. нам приходит путь типо /docx-pdf
             if (path === fullPdfList[index].link.substring(1)) {
