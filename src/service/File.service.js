7fa6ebd9d1a9a58f0666c34b2db057c1ae8fc665
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-06-23 16:09:03 +0500

    add download file funtions

--- src/service/File.service.js
+++ src/service/File.service.js
@@ -1,8 +1,18 @@
-import { $api } from "../api";
-
-
 export default class FileService {
-    static async upload(from, to, data) {
-        return await $api.post(`/from-to`, data);
+    static downloadByUrl = (url) => {
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = url.name;
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+    }
+
+    static downloadById = (id, files) => {
+        files.forEach((file) => {
+            if (file.id === id) {
+                this.downloadByUrl(file.url);
+            }
+        });
     }
 }
