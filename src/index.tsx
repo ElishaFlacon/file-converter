bd69b6c80f04de4e06f97b991bf508e0bf137819
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-06-30 18:56:49 +0500

    index.js to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,7 @@ import 'rsuite/dist/rsuite.min.css';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <App />
 );
