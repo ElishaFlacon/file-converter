ef3c68c79a1ab6a514c69c9c6880252dfa0125a7
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-07-05 23:55:42 +0500

    router.js to router.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,9 +2,10 @@ import Home from '../pages/Home/Home';
 import About from '../pages/About/About';
 import Convertation from '../pages/Convertation/Convertation';
 import Undefind from '../pages/Undefind/Undefind';
+import IRoutes from '../types/Routes';
 
 
-export const routes = [
+export const routes: IRoutes[] = [
     { path: '/', component: Home },
     { path: '/about', component: About },
     { path: '/undefind', component: Undefind },
