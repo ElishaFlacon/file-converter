337ad5e99873aeddb1d099683d631cb61768bab1
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-07-06 21:00:16 +0500

    api.js to api.ts

---
+++ src/api/index.ts
@@ -0,0 +1,9 @@
+import axios, { AxiosInstance } from "axios";
+
+
+export const API_URL: string = 'http://localhost:8000';
+
+export const $api: AxiosInstance = axios.create({
+    baseURL: API_URL,
+    withCredentials: true,
+});
