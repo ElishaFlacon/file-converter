3c58484d96f6d375cdcedb4e61bd98547df8ea83
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-07-11 10:28:52 +0500

    add login route

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,15 @@ import About from '../pages/About/About';
 import Convertation from '../pages/Convertation/Convertation';
 import Undefind from '../pages/Undefind/Undefind';
 import IRoutes from '../types/Routes';
+import Login from '../pages/Login/Login';
 
 
 export const routes: IRoutes[] = [
     { path: '/', component: Home },
     { path: '/about', component: About },
+    { path: '/login', component: Login },
+    // { path: '/registration', component: Registration },
+    // { path: '/files', component: Files },
     { path: '/undefind', component: Undefind },
     { path: '/:convert', component: Convertation },
 ]
