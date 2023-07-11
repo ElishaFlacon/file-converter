291a88102f6b5a7c32597f6f94efa72da2f10db9
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-07-11 16:24:51 +0500

    add login page

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,54 @@
+import React, { FC } from 'react';
+import { Email, CharacterLock } from '@rsuite/icons';
+import classes from './Login.module.css';
+import FormInputList from '../../components/FormInputList/FormInputList';
+import { Button } from 'rsuite';
+import { Link } from 'react-router-dom';
+
+
+const Login: FC = () => {
+    return (
+        <div className={classes.login}>
+            <FormInputList
+                data={{
+                    password: '',
+                    email: '',
+                }}
+
+                formInputs={[
+                    {
+                        name: 'email',
+                        label: <><Email /> Электронная почта</>,
+                        rest: {
+                            type: "email",
+                        },
+                    },
+                    {
+                        name: 'password',
+                        label: <><CharacterLock /> Пароль</>,
+                        rest: {
+                            type: "password",
+                            autoComplete: "off",
+                        },
+                    }
+                ]}
+
+                formButtons={[
+                    <Link to='/reset' key='0'>
+                        <Button appearance='link'>Забыли свой пароль?</Button>
+                    </Link>,
+                    <Button appearance='primary' key='1'>Войти</Button>,
+                    <div key='2'>
+                        У Вас нет учётной записи?
+                        <Link to='/registration'>
+                            <Button appearance='link'>Создать учетную запись</Button>
+                        </Link>
+                    </div>,
+                ]}
+            />
+        </div>
+    );
+}
+
+
+export default Login;
