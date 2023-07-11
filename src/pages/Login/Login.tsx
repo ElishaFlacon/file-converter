c1f408a8a72b8ce31da8f3e71be5b8baf4011329
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-07-11 17:12:54 +0500

    group button

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 import { Email, CharacterLock } from '@rsuite/icons';
 import classes from './Login.module.css';
 import FormInputList from '../../components/FormInputList/FormInputList';
-import { Button } from 'rsuite';
+import { Button, ButtonToolbar } from 'rsuite';
 import { Link } from 'react-router-dom';
 
 
@@ -34,10 +34,12 @@ const Login: FC = () => {
                 ]}
 
                 formButtons={[
-                    <Link to='/reset' key='0'>
-                        <Button appearance='link'>Забыли свой пароль?</Button>
-                    </Link>,
-                    <Button appearance='primary' key='1'>Войти</Button>,
+                    <ButtonToolbar key='0'>
+                        <Button appearance='primary'>Войти</Button>
+                        <Link to='/reset'>
+                            <Button appearance='link'>Забыли свой пароль?</Button>
+                        </Link>
+                    </ButtonToolbar>,
                     <div className={classes.center} key='2'>
                         У Вас нет учётной записи?
                         <Link to='/registration'>
