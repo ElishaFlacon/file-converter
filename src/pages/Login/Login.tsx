ef61d70fb3a017b6da1cc15cbd1e18b926d7dd64
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-07-11 18:08:19 +0500

    code redesign

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,52 +1,76 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useRef } from 'react';
 import { Email, CharacterLock } from '@rsuite/icons';
 import classes from './Login.module.css';
 import FormInputList from '../../components/FormInputList/FormInputList';
-import { Button, ButtonToolbar } from 'rsuite';
+import { Button, ButtonToolbar, FormInstance } from 'rsuite';
 import { Link } from 'react-router-dom';
+import { loginModel } from '../../models';
+import { useShowMessage } from '../../hooks/useShowMessage';
+
+
+const data = {
+    password: '',
+    email: '',
+}
+
+const formInputs = [
+    {
+        name: 'email',
+        label: <><Email /> Электронная почта</>,
+        rest: {
+            type: "email",
+        },
+    },
+    {
+        name: 'password',
+        label: <><CharacterLock /> Пароль</>,
+        rest: {
+            type: "password",
+            autoComplete: "off",
+        },
+    }
+]
 
 
 const Login: FC = () => {
+    const [formValue, setFormValue] = useState(data);
+    const formRef = useRef<FormInstance<Record<string, any>>>(null);
+    const showMessage = useShowMessage();
+
+    const handleSubmit = () => {
+        if (!formRef.current?.check()) {
+            showMessage('Заполните форму!', 'error')
+            return;
+        }
+
+        console.log(formValue, 'Form Value');
+    }
+
+    const formButtons = [
+        <ButtonToolbar key='0'>
+            <Button appearance='primary' onClick={handleSubmit}>Войти</Button>
+            <Link to='/reset'>
+                <Button appearance='link'>Забыли свой пароль?</Button>
+            </Link>
+        </ButtonToolbar>,
+        <div className={classes.center} key='2'>
+            У Вас нет учётной записи?
+            <Link to='/registration'>
+                <Button appearance='link'>Создать учетную запись</Button>
+            </Link>
+        </div>,
+    ]
+
+
     return (
         <div className={classes.login}>
             <FormInputList
-                data={{
-                    password: '',
-                    email: '',
-                }}
-
-                formInputs={[
-                    {
-                        name: 'email',
-                        label: <><Email /> Электронная почта</>,
-                        rest: {
-                            type: "email",
-                        },
-                    },
-                    {
-                        name: 'password',
-                        label: <><CharacterLock /> Пароль</>,
-                        rest: {
-                            type: "password",
-                            autoComplete: "off",
-                        },
-                    }
-                ]}
-
-                formButtons={[
-                    <ButtonToolbar key='0'>
-                        <Button appearance='primary'>Войти</Button>
-                        <Link to='/reset'>
-                            <Button appearance='link'>Забыли свой пароль?</Button>
-                        </Link>
-                    </ButtonToolbar>,
-                    <div className={classes.center} key='2'>
-                        У Вас нет учётной записи?
-                        <Link to='/registration'>
-                            <Button appearance='link'>Создать учетную запись</Button>
-                        </Link>
-                    </div>,
-                ]}
+                formValue={formValue}
+                setFormValue={setFormValue}
+                formRef={formRef}
+                formInputs={formInputs}
+                formButtons={formButtons}
+                model={loginModel}
             />
         </div>
     );
--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,56 +1,80 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useRef } from 'react';
 import { Email, CharacterLock } from '@rsuite/icons';
 import classes from './Registration.module.css';
 import FormInputList from '../../components/FormInputList/FormInputList';
-import { Button } from 'rsuite';
+import { Button, FormInstance } from 'rsuite';
 import { Link } from 'react-router-dom';
+import { registrationModel } from '../../models';
+import { useShowMessage } from '../../hooks/useShowMessage';
+
+
+const data = {
+    email: '',
+    password: '',
+    verify: '',
+}
+
+const formInputs = [
+    {
+        name: 'email',
+        label: <><Email /> Электронная почта</>,
+        rest: {
+            type: "email",
+        },
+    },
+    {
+        name: 'password',
+        label: <><CharacterLock /> Пароль</>,
+        rest: {
+            type: "password",
+            autoComplete: "off",
+        },
+    },
+    {
+        name: 'verify',
+        label: <><CharacterLock /> Повторите пароль</>,
+        rest: {
+            type: "password",
+            autoComplete: "off",
+        },
+    },
+]
 
 
 const Registration: FC = () => {
+    const [formValue, setFormValue] = useState(data);
+    const formRef = useRef<FormInstance<Record<string, any>>>(null);
+    const showMessage = useShowMessage();
+
+    const handleSubmit = () => {
+        if (!formRef.current?.check()) {
+            showMessage('Заполните форму!', 'error')
+            return;
+        }
+        
+        console.log(formValue, 'Form Value');
+    }
+
+    const formButtons = [
+        <Button appearance='primary' key='0' onClick={handleSubmit}>Регистрация</Button>,
+        <div className={classes.center} key='1'>
+            Вы уже зарегистрированы?
+            <Link to='/login'>
+                <Button appearance='link'>Войти</Button>
+            </Link>
+        </div>,
+    ]
+
+
     return (
         <div className={classes.registration}>
             <FormInputList
-                data={{
-                    email: '',
-                    password: '',
-                    verify: '',
-                }}
-
-                formInputs={[
-                    {
-                        name: 'email',
-                        label: <><Email /> Электронная почта</>,
-                        rest: {
-                            type: "email",
-                        },
-                    },
-                    {
-                        name: 'password',
-                        label: <><CharacterLock /> Пароль</>,
-                        rest: {
-                            type: "password",
-                            autoComplete: "off",
-                        },
-                    },
-                    {
-                        name: 'verify',
-                        label: <><CharacterLock /> Повторите пароль</>,
-                        rest: {
-                            type: "password",
-                            autoComplete: "off",
-                        },
-                    },
-                ]}
-
-                formButtons={[
-                    <Button appearance='primary' key='0'>Регистрация</Button>,
-                    <div className={classes.center} key='1'>
-                        Вы уже зарегистрированы?
-                        <Link to='/login'>
-                            <Button appearance='link'>Войти</Button>
-                        </Link>
-                    </div>,
-                ]}
+                formValue={formValue}
+                setFormValue={setFormValue}
+                formRef={formRef}
+                formInputs={formInputs}
+                formButtons={formButtons}
+                model={registrationModel}
             />
         </div>
     );
