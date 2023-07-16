05f6494210ff4c2f180b1d35a96d0c9765b0f857
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-07-16 22:43:51 +0500

    add user store and update buttons to header-button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,25 +3,30 @@ import { Navbar, Nav, FlexboxGrid } from 'rsuite';
 import classes from './Header.module.css';
 import Icon from '../UI/Icon/Icon';
 import FlexGridList from '../FlexGridList/FlexGridList';
-import { Link } from 'react-router-dom';
 import { fromPdfList, toPdfList } from '../../config';
+import { useTypedSelector } from '../../hooks/useTypedSelector';
+import HeaderButton from '../UI/HeaderButton/HeaderButton';
 
 
 const Header: FC = () => {
+    const { isAuth, data } = useTypedSelector(state => state.user);
+
+    const aboutText = "О нас";
+    const loginText = 'Вход';
+    const registrationText = 'Регистрация';
+    const logoutText = 'Выйти';
+
+
     return (
         <Navbar className={classes.header}>
             <div className='container'>
                 <Nav>
-                    <Link to='/'>
-                        <Nav.Item as='div'>
-                            <Icon picture={'pdf.svg'} />
-                        </Nav.Item>
-                    </Link>
-                    <Link to='/about'>
-                        <Nav.Item as='div'>
-                            О нас
-                        </Nav.Item>
-                    </Link>
+                    <HeaderButton to='/'>
+                        <Icon picture={'pdf.svg'} />
+                    </HeaderButton>
+                    <HeaderButton to='/about'>
+                        {aboutText}
+                    </HeaderButton>
                     <Nav.Menu trigger='hover' title="Конвертировать PDF">
                         <FlexboxGrid className={classes.flex_box}>
                             <FlexGridList items={toPdfList} title="Конвертация в PDF" />
@@ -30,16 +35,26 @@ const Header: FC = () => {
                     </Nav.Menu>
                 </Nav>
                 <Nav pullRight>
-                    <Link to='/login'>
-                        <Nav.Item as='div'>
-                            Вход
-                        </Nav.Item>
-                    </Link>
-                    <Link to='/registration'>
-                        <Nav.Item as='div'>
-                            Регистрация
-                        </Nav.Item>
-                    </Link>
+                    {isAuth
+                        ?
+                        <>
+                            <HeaderButton to='/profile'>
+                                {data?.user.username}
+                            </HeaderButton>
+                            <HeaderButton to='/logout'>
+                                {logoutText}
+                            </HeaderButton>
+                        </>
+                        :
+                        <>
+                            <HeaderButton to='/login'>
+                                {loginText}
+                            </HeaderButton>
+                            <HeaderButton to='/registration'>
+                                {registrationText}
+                            </HeaderButton>
+                        </>
+                    }
                 </Nav>
             </div>
         </Navbar>
