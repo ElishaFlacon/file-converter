75c3af81df906a35ae5ddda4e99b8cc77d8438df
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-07-16 22:46:58 +0500

    update condition

--- src/service/Path.service.ts
+++ src/service/Path.service.ts
@@ -9,8 +9,7 @@ export default class PathService {
     // чтобы не было переходов по ссылкам для которых нет функционала, типо pdf-pdf
     static validation: V = (path) => {
         for (let index = 0; index < fullPdfList.length; index++) {
-            // удаляем 1 букву, т.к. нам приходит путь типо /docx-pdf
-            if (path === fullPdfList[index].link.substring(1)) {
+            if (path === fullPdfList[index].link.split('/').pop()) {
                 return true;
             }
         }
