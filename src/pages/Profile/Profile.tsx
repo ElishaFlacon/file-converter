802359321018e3ecddf3811f3e81cc8490921133
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-07-27 20:47:22 +0500

    add profile

---
+++ src/pages/Profile/Profile.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react'
+import Text from '../../components/UI/Text/Text';
+
+const Profile: FC = () => {
+
+
+    return (
+        <div>
+            <Text size='df'>В профиле вы можете скачать свои старые файлы</Text>
+            <Text size='sm'>Пока что не работает</Text>
+        </div>
+    );
+}
+
+
+export default Profile;
