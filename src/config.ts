71f660116125e5b5d3c12eb94522bff5629ecba5
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-07-29 13:02:38 +0500

    update api

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import IAccepts from './types/Accepts';
 import IСonversionItem from './types/СonversionItem';
 
 
-export const API_URL: string = 'http://1627461-cg09372.twc1.net:8000';
+export const API_URL: string = 'https://1627461-cg09372.twc1.net';
 
 export const toPdfList: IСonversionItem[] = [
     {
