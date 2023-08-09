6cbde13697715b2b825c4415f01e6aaa9da4c6f3
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-08-10 01:49:50 +0500

    update init-state, save, get, remove user

--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -1,4 +1,4 @@
-import { UserActionTypes, IUserState, UserAction } from "../../types/User";
+import { UserActionTypes, IUserState, UserAction, IUser } from "../../types/User";
 
 
 const initalState: IUserState = {
@@ -6,57 +6,60 @@ const initalState: IUserState = {
     data: null,
     loading: false,
     error: null,
+    message: null,
 }
 
-const saveUser = (user: string) => {
-    localStorage.setItem('user', user);
+const saveUser = (user: IUser) => {
+    localStorage.setItem('email', user.email);
+    localStorage.setItem('username', user.username);
+    localStorage.setItem('country', user.country);
     localStorage.setItem('isAuth', 'true');
 }
 
 const getUser = () => {
-    const user = localStorage.getItem('user') || '';
-    const auth = localStorage.getItem('isAuth') || '';
-    return [user, auth]
+    const email = localStorage.getItem('email') || '';
+    const username = localStorage.getItem('username') || '';
+    const country = localStorage.getItem('country') || '';
+    const isAuth = localStorage.getItem('isAuth') || '';
+    return [email, username, country, isAuth];
 }
 
 const removeUser = () => {
-    localStorage.removeItem('user')
-    localStorage.removeItem('isAuth');
+    localStorage.clear();
 }
 
 
 export const userReducer = (state: IUserState = initalState, action: UserAction): IUserState => {
     switch (action.type) {
         case UserActionTypes.USER_LOGIN:
-            return { isAuth: false, loading: true, error: null, data: null };
+            return { isAuth: false, loading: true, error: null, data: null, message: null };
         case UserActionTypes.USER_LOGIN_SUCCESS:
-            saveUser(action.payload.user.username);
-            return { isAuth: true, loading: false, error: null, data: action.payload };
+            saveUser(action.payload.user);
+            return { isAuth: true, loading: false, error: null, data: action.payload, message: null };
         case UserActionTypes.USER_LOGIN_ERROR:
-            return { isAuth: false, loading: false, error: action.payload, data: null };
+            return { isAuth: false, loading: false, error: action.payload, data: null, message: null };
 
         case UserActionTypes.USER_REGISTRATION:
-            return { isAuth: false, loading: true, error: null, data: null };
+            return { isAuth: false, loading: true, error: null, data: null, message: null };
         case UserActionTypes.USER_REGISTRATION_SUCCESS:
-            saveUser(action.payload.user.username);
-            return { isAuth: true, loading: false, error: null, data: action.payload };
+            return { isAuth: false, loading: false, error: null, data: null, message: action.payload.message };
         case UserActionTypes.USER_REGISTRATION_ERROR:
-            return { isAuth: false, loading: false, error: action.payload, data: null };
+            return { isAuth: false, loading: false, error: action.payload, data: null, message: null };
 
         case UserActionTypes.USER_LOGOUT:
             removeUser();
-            return { isAuth: false, loading: false, error: null, data: null };
+            return { isAuth: false, loading: false, error: null, data: null, message: null };
 
         case UserActionTypes.USER_CHECK_AUTH:
-            const [user, auth] = getUser();
-            const data = { access_token: '', user: { email: user, username: user } };
-            return { isAuth: auth ? Boolean(auth) : false, loading: true, error: null, data: user ? data : null };
+            const [email, username, country, isAuth] = getUser();
+            const data = { access_token: '', user: { email, username, country } };
+            return { isAuth: isAuth ? Boolean(isAuth) : false, loading: true, error: null, data: email ? data : null, message: null };
         case UserActionTypes.USER_CHECK_AUTH_SUCCESS:
-            saveUser(action.payload.user.username);
-            return { isAuth: true, loading: false, error: null, data: action.payload };
+            saveUser(action.payload.user);
+            return { isAuth: true, loading: false, error: null, data: action.payload, message: null };
         case UserActionTypes.USER_CHECK_AUTH_ERROR:
             removeUser();
-            return { isAuth: false, loading: false, error: action.payload, data: null };
+            return { isAuth: false, loading: false, error: action.payload, data: null, message: null };
 
         default:
             return state;
