7463cde1ac50d46ec62f21663571be588e5928f2
Elisha Flacon <thefucingsokol.gg@yandex.ru>
2023-08-10 16:40:50 +0500

    add confirm password and confirm profile model

--- src/models/index.ts
+++ src/models/index.ts
@@ -38,3 +38,24 @@ export const registrationModel = Schema.Model({
         }, passwordsNotMatch)
         .isRequired(requiredField)
 });
+
+export const confirmPasswordModel = Schema.Model({
+    password: StringType().isRequired(requiredField),
+    verify: StringType()
+        .addRule((value, data) => {
+            if (value !== data.password) {
+                return false;
+            }
+            return true;
+        }, passwordsNotMatch)
+        .isRequired(requiredField)
+});
+
+export const confirmProfileModel = Schema.Model({
+    username: StringType()
+        .maxLength(32, lengthWrong)
+        .isRequired(requiredField),
+    email: StringType()
+        .isEmail(emailWrong)
+        .isRequired(requiredField),
+})
